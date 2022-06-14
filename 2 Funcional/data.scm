30c37064676a13ee584cf4a78680ed713179b0a4
Nicolas <nhormazabal@geovictoria.com>
2022-06-13 23:47:42 -0400

    dato ejemplo 2 funcional

---
+++ 2 Funcional/data.scm
@@ -0,0 +1,15 @@
+(define a (list 10 2 3 54 6 9 7 10))
+
+(define b (list 5 1 8 6 5 5 4 77 8 6))
+
+(define c (list 80 80 80))
+
+(define e (list 99 156 54 48 99))
+
+(define d (list 16 94 75 30 56 16 8 16))
+
+(define uno (list a b c))
+
+(define dos (list c e b a))
+
+(define tres (list b c d e))
